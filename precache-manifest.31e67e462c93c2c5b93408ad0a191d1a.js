81f9cfa8bf91e7d99f8db4279fb390abd2b68aed
malith-gammanpila <malith@boox.no>
2020-04-08 20:02:38 +0530

    Updates

---
+++ precache-manifest.31e67e462c93c2c5b93408ad0a191d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f0278b3e3baea61bca3636aef86be9",
+    "url": "/corona-pandemic/index.html"
+  },
+  {
+    "revision": "67a7bc274eda3348cc2a",
+    "url": "/corona-pandemic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c79ec1a9cf423332a149",
+    "url": "/corona-pandemic/static/js/2.4237e98f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-pandemic/static/js/2.4237e98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a7bc274eda3348cc2a",
+    "url": "/corona-pandemic/static/js/main.4d322e05.chunk.js"
+  },
+  {
+    "revision": "de2c4e39464a4c0ad623",
+    "url": "/corona-pandemic/static/js/runtime-main.b837e69b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona-pandemic/static/media/logo.5d5d9eef.svg"
+  }
+]);
